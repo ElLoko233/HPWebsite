6dc0a17e64d39727f1d3b8ba188114b4c26bd04e
Lelethu Futshane <ellokoprograma@gmail.com>
2021-12-08 10:58:42 +0200

    updated docs folder

---
+++ docs/js/custom/carouselImages.js
@@ -0,0 +1,19 @@
+import { tns } from "../../../node_modules/tiny-slider/dist/tiny-slider"
+
+var slider = tns({
+  container: '.myslider',
+  items: 1,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2
+    },
+    700: {
+      gutter: 30
+    },
+    900: {
+      items: 3
+    }
+  }
+});
