259bbaff146cc1096131997b2bdc0266ba2652c2
Lelethu Futshane <ellokoprograma@gmail.com>
2022-01-15 10:34:10 +0200

    changed tinyslider plugin for slick plugin

--- Public/js/custom/carouselImages.js
+++ Public/js/custom/carouselImages.js
@@ -1,19 +1,35 @@
-import { tns } from "../../../node_modules/tiny-slider/dist/tiny-slider"
-
-var slider = tns({
-  container: '.myslider',
-  items: 1,
-  responsive: {
-    640: {
-      edgePadding: 20,
-      gutter: 20,
-      items: 2,
-    },
-    700: {
-      gutter: 25,
-    },
-    900: {
-      items: 3,
-    },
-  }
+$(document).ready(function(){
+  $('.myslider').slick({
+    "autoplay": true,
+    "arrows": false,
+    "centerMode": true,
+    "mobileFirst": true,
+    "centerPadding": '60px',
+    "responsive": [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            "centerPadding": '60px',
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            "centerPadding": '60px',
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            "centerPadding": '60px',
+          }
+      }
+    ]
+  });
 });
